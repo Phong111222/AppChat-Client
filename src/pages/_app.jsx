abfd00193729dd494d096e5e4659ff10bf558fed
Tien Phong <tienphong111222@gmail.com>
2021-07-26 15:49:15 +0700

    config redux persist

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,30 +1,32 @@
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import ChatLayout from '../components/app/ChatLayout';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import AuthBackground from '../components/Auth/Background';
 import WrappedAuth from '../components/Auth/WrappedAuth';
-import wrapper from '../store';
-
+import wrapper, { persistor, store } from '../store';
+import { PersistGate } from 'redux-persist/integration/react';
 function MyApp({ Component, pageProps }) {
   const { pathname } = useRouter();
   return (
-    // <Provider store={store}>
-    <ChakraProvider>
-      <CSSReset />
-      {pathname === '/register' || pathname === '/' ? (
-        <AuthBackground>
-          <Component {...pageProps} />
-        </AuthBackground>
-      ) : (
-        <WrappedAuth>
-          <ChatLayout>
-            <Component {...pageProps} />
-          </ChatLayout>
-        </WrappedAuth>
-      )}
-    </ChakraProvider>
-    // </Provider>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <ChakraProvider>
+          <CSSReset />
+          {pathname === '/register' || pathname === '/' ? (
+            <AuthBackground>
+              <Component {...pageProps} />
+            </AuthBackground>
+          ) : (
+            <WrappedAuth>
+              <ChatLayout>
+                <Component {...pageProps} />
+              </ChatLayout>
+            </WrappedAuth>
+          )}
+        </ChakraProvider>
+      </PersistGate>
+    </Provider>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,26 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import RootReducer from './rootReducer';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { persistReducer, persistStore } from 'redux-persist';
+const persistConfig = {
+  key: 'root',
+
+  storage,
+  stateReconciler: autoMergeLevel2,
+  whitelist: ['auth', 'user'],
+};
+
+const persistedReudcer = persistReducer(persistConfig, RootReducer);
+
 const composeEnhancer = composeWithDevTools({ trace: true });
 
 export const store = createStore(
-  RootReducer,
+  persistedReudcer,
   composeEnhancer(applyMiddleware(thunk))
 );
+export const persistor = persistStore(store);
 
 const wrapper = createWrapper(() => store);
 
