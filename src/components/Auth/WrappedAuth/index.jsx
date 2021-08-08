c71ac3f7e5f47872223dffc2e69d5e508cb32547
Tien Phong <tienphong111222@gmail.com>
2021-08-08 22:28:03 +0700

    verify token

--- src/components/Auth/WrappedAuth/index.jsx
+++ src/components/Auth/WrappedAuth/index.jsx
@@ -1,14 +1,28 @@
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import useAuth from '../../../hook/useAuth';
-
+import jsonwebtoken from 'jsonwebtoken';
+import { useDispatch } from 'react-redux';
+import { Logout } from '../../../store/Auth/action';
 export default function WrappedAuth({ children }) {
+  const dispatch = useDispatch();
   const route = useRouter();
   const [jwt] = useAuth();
   useEffect(() => {
     if (!jwt) {
       route.push('/');
     }
+  }, [route.pathname]);
+  useEffect(() => {
+    jsonwebtoken.verify(
+      jwt,
+      '22EC4C46D29738BD9066E80D89390BF97B2091B71BDEEA04EFDD51574D283B10',
+      (err, _) => {
+        if (err) {
+          dispatch(Logout(route));
+        }
+      }
+    );
   }, [route.pathname]);
   return <>{children}</>;
 }
