eea702ffc0c11359bd34addce7f5b6aefa4690d1
Tien Phong <tienphong111222@gmail.com>
2021-08-08 22:32:56 +0700

    cmt logout component

--- src/components/Auth/Logout/index.jsx
+++ src/components/Auth/Logout/index.jsx
@@ -1,7 +1,7 @@
-import { useRouter } from 'next/router';
-import React from 'react';
+// import { useRouter } from 'next/router';
+// import React from 'react';
 
 export default function index() {
-  const router = useRouter();
+  // const router = useRouter();
   return <div>Logout</div>;
 }
